0d3c9de082aac812b34e0325ad9cf88a2f519683
dgzornoza <dgzornoza@dgzornoza.com>
2024-05-15 21:12:15 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,46 @@
-// #include <SDL2/SDL.h>
-// #ifdef __EMSCRIPTEN__
-//     #include <emscripten.h>
-// #endif
-// #include <stdlib.h>
+#include <SDL2/SDL.h>
+#ifdef __EMSCRIPTEN__
+    #include <emscripten.h>
+#endif
+#include <stdlib.h>
 
-// SDL_Window *window;
-// SDL_Renderer *renderer;
-// SDL_Surface *surface;
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Surface *surface;
 
-// void drawRandomPixels() {
-//     if (SDL_MUSTLOCK(surface)) SDL_LockSurface(surface);
+void drawRandomPixels() {
+    if (SDL_MUSTLOCK(surface)) SDL_LockSurface(surface);
 
-//     Uint8 * pixels = (Uint8*)surface->pixels;
+    Uint8 * pixels = (Uint8*)surface->pixels;
 
-//     for (int i=0; i < 1048576; i++) {
-//         char randomByte = rand() % 255;
-//         pixels[i] = randomByte;
-//     }
+    for (int i=0; i < 1048576; i++) {
+        char randomByte = rand() % 255;
+        pixels[i] = randomByte;
+    }
 
-//     if (SDL_MUSTLOCK(surface)) SDL_UnlockSurface(surface);
+    if (SDL_MUSTLOCK(surface)) SDL_UnlockSurface(surface);
 
-//     SDL_Texture *screenTexture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Texture *screenTexture = SDL_CreateTextureFromSurface(renderer, surface);
 
-//     SDL_RenderClear(renderer);
-//     SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
-//     SDL_RenderPresent(renderer);
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, screenTexture, NULL, NULL);
+    SDL_RenderPresent(renderer);
 
-//     SDL_DestroyTexture(screenTexture);
-// }
+    SDL_DestroyTexture(screenTexture);
+}
 
-// int main(int argc, char* argv[]) {
-//     SDL_Init(SDL_INIT_VIDEO);
-//     SDL_CreateWindowAndRenderer(512, 512, 0, &window, &renderer);
+int main(int argc, char* argv[]) {
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(512, 512, 0, &window, &renderer);
 
-//     surface = SDL_CreateRGBSurface(0, 512, 512, 32, 0, 0, 0, 0);
+    surface = SDL_CreateRGBSurface(0, 512, 512, 32, 0, 0, 0, 0);
 
-//     #ifdef __EMSCRIPTEN__
-//     emscripten_set_main_loop(drawRandomPixels, 0, 1);
-//     #else
-//     while(1) {
-//         drawRandomPixels();
-//         SDL_Delay(16);
-//     }
-//     #endif
-// }
+    #ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(drawRandomPixels, 0, 1);
+    #else
+    while(1) {
+        drawRandomPixels();
+        SDL_Delay(16);
+    }
+    #endif
+}
